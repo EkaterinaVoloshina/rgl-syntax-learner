24b8e48938385756002df464c98533da741aa1d8
EkaterinaVoloshina <vokat@mail.ru>
2025-03-17 11:22:25 +0100

    integrate morphology

--- syntax_learner/utils.py
+++ syntax_learner/utils.py
@@ -1,13 +1,27 @@
 import re
+from collections import defaultdict
 
-filepath = "../gf-rgl/src/macedonian/ResMkd.gf"
+lang2gf = {"macedonian": "Mkd",
+           "albanian": "Sqa",
+           "kazakh" : "Kaz"}
 
-with open(filepath, "r") as f:
-    text = f.read().splitlines()
 
-params = [t for t in text if t.startswith("param")]
+def getParams(lang):
+    lang = lang.lower()
+    if lang in lang2gf:
+        filepath = f"../gf-rgl/src/{lang}/Res{lang2gf[lang]}.gf"
+    else:
+        print(f"{lang} is not supported in GF")
+        return None
 
-for p in params:
-    par, vals = re.findall(r"param (.+) = (.*) ;", p)[0]
-    vals = [v.split()[0] for v in vals.split(" | ")]
-    print(par, vals)
+    with open(filepath, "r") as f:
+        text = f.read().splitlines()
+
+    params = [t for t in text if t.startswith("param")]
+
+    paramDict = defaultdict()
+    for p in params:
+        par, vals = re.findall(r"param (.+) = (.*) ;", p)[0]
+        vals = [v.split()[0] for v in vals.split(" | ")]
+        paramDict[par] = vals
+    return paramDict
