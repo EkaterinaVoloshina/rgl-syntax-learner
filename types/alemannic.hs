05edd093489afa87dbe78ce1f059805ee651b7b9
Krasimir Angelov <kr.angelov@gmail.com>
2026-02-02 10:13:25 +0100

    inject singular when necessary

--- types/alemannic.hs
+++ types/alemannic.hs
@@ -5,6 +5,13 @@ main = learnerMain ((defaultConfig "gsw" "gsw" "alemannic")
                            \lemma pos forms ->
                                 case pos of
                                   "noun" -> (["singular"],lemma) : forms
+                                  "adj"  -> map insertSingular forms
                                   _      -> forms
                        , cfgTreebanks = ["SUD_Alemannic-DIVITAL"]
                        })
+  where
+    insertSingular (tags,w)
+      | (elem "strong" tags || elem "weak" tags) &&
+        not (elem "plural" tags)
+                  = ("singular":tags,w)
+      | otherwise = (tags,w)
